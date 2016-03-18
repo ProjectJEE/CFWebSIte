b09ca2536c26b4fba385a20d1478ddefa924cec9
ProjectJEE <ibtissam.naji10@gmail.com>
2016-03-18 15:15:05 +0100

    updated DAO

--- src/ma/lhope/daos/UserDao.java
+++ src/ma/lhope/daos/UserDao.java
@@ -1,9 +1,13 @@
-package ma.lhope.daos;
+package dao;
 
-import ma.lhope.models.User;
+import entities.User;
 
 public interface UserDao {
 
 	public void create(User entity);
-	// public void update(User entity);
+//	public void update(User entity);
+	public void delete(User entity);
+	public User getUserById(long id);
+	public User getUserByName(String name);
+
 }
--- src/ma/lhope/daos/UserDaoImpl.java
+++ src/ma/lhope/daos/UserDaoImpl.java
@@ -1,9 +1,14 @@
-package ma.lhope.daos;
+package dao;
 
+
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-import ma.lhope.models.User;
+import entities.User;
+import utils.HibernateUtils;
 
 public class UserDaoImpl implements UserDao {
 
@@ -11,14 +16,67 @@ public class UserDaoImpl implements UserDao {
 		Session s = HibernateUtils.getSessionFactory().openSession();
 		Transaction tx = s.beginTransaction();
 		try {
+
 			s.persist(entity);
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
-		} finally {
+		}finally {
 			s.close();
 		}
 
+	}
+
+//	public void update(User entity) {
+//
+//	}
+	public void delete(User entity) {
+		Session s = HibernateUtils.getSessionFactory().openSession();
+		Transaction tx = s.beginTransaction();
+		try {
+
+			s.delete(entity);
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		}finally {
+			s.close();
+		}
+	}//end delete
+		  
+	
+	//@SuppressWarnings("unchecked")
+	public User getUserById(long id)
+	{
+		User user=null;
+		Session s = HibernateUtils.getSessionFactory().openSession();
+		Transaction tx = s.beginTransaction();
+		try {
+		user=s.get(User.class, id);
+		//user= s.byId( User.class ).load( id );
+		tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+			e.printStackTrace();
+		}finally {
+			s.close();
+		}
+		return user;}
+	
+	public User getUserByName(String name)
+	{
+		//http://www.jmdoudoux.fr/java/dej/chap-hibernate.htm
+//		Query q = s.createQuery("from foo Foo as foo where foo.name=:name and foo.size=:size");
+//		q.setProperties(fooBean); // fooBean has getName() and getSize()
+//		List foos = q.list();
+		Session s = HibernateUtils.getSessionFactory().openSession();
+		Transaction tx = s.beginTransaction();
+		Query q = s.createQuery("from User u where u.name_user=:name");
+		q.setString("name", name);
+		List<User> users=q.list();
+		if(users.size()==0)
+			return null;
+		return users.get(0);
 	}
 
 }
